3bc39b4b6f06c2b3a2a7fe169969d4226127159b
andrewjbeery <124631837+andrewjbeery@users.noreply.github.com>
2024-11-07 00:08:55 -0600

    fixed renewing token issue

--- helpers.py
+++ helpers.py
@@ -118,15 +118,18 @@ def email_create(sender, to, holdings):
     SCOPES = ['https://www.googleapis.com/auth/gmail.send']
     historic =generate_data(holdings)
     def get_credentials():
-        """Authenticate and return Gmail API credentials."""
         creds = None
         if os.path.exists('token.json'):
             creds = Credentials.from_authorized_user_file('token.json', SCOPES)
         if not creds or not creds.valid:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-            with open('token.json', 'w') as token:
-                token.write(creds.to_json())
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request()) # type: ignore
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
+                creds = flow.run_local_server(port=0)
+                # Save the credentials with refresh token
+                with open('token.json', 'w') as token:
+                    token.write(creds.to_json())
         return creds
 
     def create_message(sender, to, image_dir):
